457cd501b69e1b9ddd4fda8e3356ad07219b3ee2
Alex Curran <haletas88@gmail.com>
2025-11-16 21:38:29 +1300

    Add "slot" random for gambling

--- text.py
+++ text.py
@@ -1,3 +1,6 @@
+import asyncio
+import random
+
 import requests
 from bot import bot
 
@@ -11,3 +14,23 @@ async def repeat(ctx, message: str, times: int = 1):
 async def quote(ctx):
     quoteData = requests.get("https://dummyjson.com/quotes/random")
     await ctx.send(f"{quoteData.json()["quote"]} - {quoteData.json()["author"]}")
+
+@bot.command()
+async def slot(ctx, money: int = 100):
+    options = ['7️⃣', '🔔', '🍒', '🍇', '🍋']
+    await ctx.send(f"Spinning with ${money}...")
+    await asyncio.sleep(1)
+
+    roll = ""
+    for i in range(3): roll += random.choice(options)
+
+    await ctx.send(roll)
+
+    if roll[0] == roll[1] and roll[1] == roll[2]:
+        money *= 10
+    elif roll[0] == roll[1] or roll[1] == roll[2] or roll[0] == roll[2]:
+        money *= 2
+    else:
+        money //= 2
+
+    await ctx.send(f"You now have ${money}!")
